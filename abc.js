8cdab38deb0ba476999af6d39ba4a7cfd4e7f5fb
Debasis Das <debasis.das@eximietas.design>
2024-01-08 11:28:59 +0530

    feat(api): add new readFile writeFile Introduce a new file handling concept Refs: #[IC-12345]

---
+++ abc.js
@@ -0,0 +1,43 @@
+// Checkig the data of the promises
+// const fetchPromise = fetch(
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+// );
+
+// console.log(fetchPromise);
+
+// fetchPromise.then((response) => {
+//   console.log(`Received response: ${response}`);
+//   return response.json();
+// }).then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+// console.log("Started request…");
+
+
+
+
+// Promise.all for cheking if all the promises are working fine or not
+
+const fetchPromise1 = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+  );
+  const fetchPromise2 = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+  );
+  const fetchPromise3 = fetch(
+    "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+  );
+  
+  Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+    .then((responses) => {
+      for (const response of responses) {
+        console.log(`${response.url}: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.error(`Failed to fetch: ${error}`);
+    });
+  
